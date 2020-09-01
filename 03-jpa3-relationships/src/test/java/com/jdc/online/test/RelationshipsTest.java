bc9b5ebe25400f6f160ccfef8114506982c7db41
DELL <DELL@wzo>
2020-09-01 09:59:30 +0630

    Share Project

---
+++ 03-jpa3-relationships/src/test/java/com/jdc/online/test/RelationshipsTest.java
@@ -0,0 +1,29 @@
+package com.jdc.online.test;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class RelationshipsTest {
+	
+	static EntityManagerFactory EMF;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		EMF = Persistence.createEntityManagerFactory("relationships");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		EMF.close();
+	}
+
+	@Test
+	void test() {
+		
+	}
+
+}
